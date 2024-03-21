2f2d41899e13013ee30bf6ea901fde1e99abc788
Grenell <TTGrenell@gmail.com>
2024-03-21 11:27:59 -0400

    Fragment and MainActivity now utilize the ViewModel

--- app/src/main/java/edu/temple/diceroll/DiceFragment.kt
+++ app/src/main/java/edu/temple/diceroll/DiceFragment.kt
@@ -36,7 +36,8 @@ class DiceFragment : Fragment() {
             findViewById<Button>(R.id.rollButton).setOnClickListener {
 
                 // Generate random number
-                (Random.nextInt(sides!!) + 1).toString()
+                val roll = (Random.nextInt(sides!!) + 1)
+                viewModel.setDiceRoll(roll)
             }
         }
     }
--- app/src/main/java/edu/temple/diceroll/DiceViewModel.kt
+++ app/src/main/java/edu/temple/diceroll/DiceViewModel.kt
@@ -1,7 +1,17 @@
 package edu.temple.diceroll
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DiceViewModel : ViewModel() {
-    
+    private val diceRoll = MutableLiveData<Int>()
+
+    fun setDiceRoll(roll: Int) {
+        diceRoll.value = roll
+    }
+
+    fun getDiceRoll() : LiveData<Int> {
+        return diceRoll
+    }
 }
